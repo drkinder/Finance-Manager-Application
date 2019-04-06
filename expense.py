4bc560b78dbf8e608e8ae1f3f4257920e2b2f7fb
Dylan <dylkinder@gmail.com>
2019-04-06 18:19:15 -0400

    Added list_widget_format to add proper spacing for QListWidget

--- expense.py
+++ expense.py
@@ -9,3 +9,14 @@ class Expense:
         self.description = db_data_list[2]
         self.category = db_data_list[3]
         self.date = db_data_list[4]
+
+    def list_widget_format(self):
+        amount = self.format_list_wiget(self.amount, 12)
+        description = self.format_list_wiget(self.description, 22)
+        category = self.format_list_wiget(self.category, 20)
+        date = self.format_list_wiget(self.date, 12)
+
+        return f"{amount}{description}{category}{date}"
+
+    def format_list_wiget(self, data, max_length):
+        return f"{data}{''*(max_length-len(data))}"
